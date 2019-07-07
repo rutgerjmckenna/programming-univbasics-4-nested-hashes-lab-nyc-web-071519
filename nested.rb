4ac3699597d913d034fc70d522483db4155adf40
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-07 06:17:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -41,7 +41,7 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
-  programmer_hash[:dennis_ritchie][]
+  programmer_hash[:dennis_ritchie][:languages]
 	programmer_hash = 
  		{
         :grace_hopper => {
