6aa98fd97f561b2e194afec141c5fc5a5b3300c5
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-07 06:23:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,6 +109,8 @@ def changing_alan
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      
+  return programmer_hash
+     
      
 end
 
@@ -128,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     dennis_new_info = 
 
 end
